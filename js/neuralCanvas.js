0247ee32d4ccb97bdbceaf7c21bb488e18d9bb60
GmanF10 <Gmankilla10@gmail.com>
2025-05-23 01:48:12 -0500

    oefeasedfa

--- js/neuralCanvas.js
+++ js/neuralCanvas.js
@@ -31,7 +31,7 @@ function resize() {
 function getRandomVelocity() {
   let speed;
   do {
-    speed = (Math.random() - 0.5) * SPEED_MULTIPLIER;
+    speed = (Math.random() - 0.1) * SPEED_MULTIPLIER;
   } while (Math.abs(speed) < SPEED_MULTIPLIER / 6); // ensure it's not too slow
   return speed;
 }
